5297f3c9f79d4a6b5f0ac99b6ca7ba5653a14b28
Krabgalasch <krabgalasch@gmail.com>
2024-06-05 21:18:28 +0200

    added different types of noise

--- src/features.py
+++ src/features.py
@@ -6,7 +6,45 @@ from torchvision import models, transforms
 from torchvision.models.inception import Inception_V3_Weights
 from PIL import Image
 import pickle
+import numpy as np
 
+def add_salt_pepper_noise(image, noise_level):
+    # Salt and pepper noise
+    noisy_image = image.clone().detach()
+    total_pixels = image.numel()
+    num_salt = np.ceil(noise_level * total_pixels * 0.5)
+    num_pepper = np.ceil(noise_level * total_pixels * 0.5)
+
+    # Add Salt noise
+    coords = [np.random.randint(0, i - 1, int(num_salt)) for i in image.shape]
+    noisy_image[coords] = 1
+
+    # Add Pepper noise
+    coords = [np.random.randint(0, i - 1, int(num_pepper)) for i in image.shape]
+    noisy_image[coords] = 0
+
+    return noisy_image
+
+def add_poisson_noise(image):
+    # Poisson noise
+    vals = len(np.unique(image))
+    vals = 2 ** np.ceil(np.log2(vals))
+    noisy_image = np.random.poisson(image * vals) / float(vals)
+    return noisy_image
+
+def add_noise(image, noise_type, noise_level):
+    if noise_level == 0.0:
+        return image
+    if noise_type == 'salt_pepper':
+        return add_salt_pepper_noise(image, noise_level)
+    elif noise_type == 'poisson':
+        return add_poisson_noise(image)
+    else:
+        return image
+
+def show_image(image):
+    img = transforms.ToPILImage()(image)
+    img.show()
 
 def add_noise(image, noise_level):
     if noise_level == 0.0:
